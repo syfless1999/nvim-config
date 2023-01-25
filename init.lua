9a69b02c9a6edaa48ed3ee52c72bfe10d572ccd2
syfless <syfless1999@gmail.com>
2023-01-25 23:25:22 +0800

    feat: first commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("syfless.core.options")
+require("syfless.core.keymaps")
+require("syfless.core.colorscheme")
---
+++ lua/syfless/core/options.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+
+opt.relativenumber = true
+opt.number = true
+
+vim.api.nvim_exec ('language en_US', true)
+
