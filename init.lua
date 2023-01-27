35963a8076c5b930441872d9584d440b47de1784
syfless <syfless1999@gmail.com>
2023-01-27 23:16:37 +0800

    feat: add cores [colorscheme, keymap, option]

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+require("syfless.plugins-setup");
 require("syfless.core.options")
 require("syfless.core.keymaps")
 require("syfless.core.colorscheme")
--- lua/syfless/core/options.lua
+++ lua/syfless/core/options.lua
@@ -5,5 +5,44 @@ local opt = vim.opt -- for conciseness
 opt.relativenumber = true
 opt.number = true
 
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- apperance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
+
+
+
+
+
+
+-- language
 vim.api.nvim_exec ('language en_US', true)
 
