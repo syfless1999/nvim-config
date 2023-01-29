1241cf8cfe5de8fc92f377c0b453e345f5073686
syfless <syfless1999@gmail.com>
2023-01-29 17:14:32 +0800

    feat: some plugins

--- init.lua
+++ init.lua
@@ -1,10 +1,20 @@
 require("syfless.plugins-setup")
+
 require("syfless.core.options")
 require("syfless.core.keymaps")
 require("syfless.core.colorscheme")
+
 require("syfless.plugins.comment")
 require("syfless.plugins.nvim-tree")
 require("syfless.plugins.lualine")
 require("syfless.plugins.telescope")
+require("syfless.plugins.nvim-cmp")
 
+require("syfless.plugins.lsp.mason")
+require("syfless.plugins.lsp.lspsaga")
+require("syfless.plugins.lsp.lspconfig")
+require("syfless.plugins.lsp.null-ls")
 
+require("syfless.plugins.autopairs")
+require("syfless.plugins.treesitter")
+require("syfless.plugins.gitsigns")
