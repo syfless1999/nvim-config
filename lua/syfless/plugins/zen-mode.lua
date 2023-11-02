c6d63d746d51d4fbcb1bc427ae111ad852c3df0d
fengshun <fengshun.syf@alibaba-inc.com>
2023-11-02 10:23:10 +0800

    fix: zen-mode hotkey

--- lua/syfless/plugins/zen-mode.lua
+++ lua/syfless/plugins/zen-mode.lua
@@ -5,6 +5,6 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set("n", "z", "<cmd>ZenMode<CR>", { desc = "Switch to Zen-Mode" })
+    keymap.set("n", "<leader>z", "<cmd>ZenMode<CR>", { desc = "Switch to Zen-Mode" })
   end,
 }
