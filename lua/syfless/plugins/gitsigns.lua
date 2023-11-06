adb1bdc96938f128964f641f834a80501dad9b9b
fengshun <fengshun.syf@alibaba-inc.com>
2023-11-06 17:53:22 +0800

    fix: gitsigns toggle current line blame

--- lua/syfless/plugins/gitsigns.lua
+++ lua/syfless/plugins/gitsigns.lua
@@ -2,8 +2,6 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("gitsigns").setup({
-      current_line_blame = true,
-    })
+    require("gitsigns").setup()
   end,
 }
