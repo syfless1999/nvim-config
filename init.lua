e3cfbb3e9709fdc3cec782e903b576ab2429c9bd
fengshun <fengshun.syf@alibaba-inc.com>
2023-11-13 11:30:12 +0800

    fix: fix sth

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
+-- nvim-tree disable netrw: `-h nvim-tree-netrw`
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("syfless.core")
 require("syfless.lazy")
--- lua/syfless/plugins/nvim-tree.lua
+++ lua/syfless/plugins/nvim-tree.lua
@@ -25,10 +25,10 @@ return {
         },
         icons = {
           glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
+            -- folder = {
+            --   arrow_closed = "", -- arrow when folder is closed
+            --   arrow_open = "", -- arrow when folder is open
+            -- },
           },
         },
       },
@@ -54,8 +54,8 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+    keymap.set("n", "<leader>et", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Focus to current file" }) -- focus to current file
   end,
 }
