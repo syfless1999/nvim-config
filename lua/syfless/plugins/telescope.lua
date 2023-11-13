c884683d61f56c659c6f7b49015ede100ba98883
fengshun <fengshun.syf@alibaba-inc.com>
2023-11-13 20:18:31 +0800

    feat: telescope telescope-live-grep-args

--- lua/syfless/plugins/telescope.lua
+++ lua/syfless/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-live-grep-args.nvim",
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
@@ -24,13 +25,19 @@ return {
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("live_grep_args")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fs",
+      ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      { desc = "Find string in cwd" }
+    )
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
   end,
 }
