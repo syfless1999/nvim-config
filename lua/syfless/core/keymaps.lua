62b40306970ef3bb995ef1b229934aeabaf25cad
syfless <syfless1999@gmail.com>
2023-11-22 20:11:33 +0800

    fix: keymaps <leader>to => :tabnew %

--- lua/syfless/core/keymaps.lua
+++ lua/syfless/core/keymaps.lua
@@ -22,13 +22,10 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>to", ":tabnew %<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<C-[>", ":tabp<CR>") --  go to previous tab
 keymap.set("n", "<C-]>", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>nu", ":set relativenumber!<CR>") -- toggle relativenumber
 
 -- plugin keymaps
-
--- vim-maxmizer
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
