99e825f93bda55461b5444fcb4729bdcf3898cf0
syfless1999 <syfless1999@gmail.com>
2023-11-23 10:51:29 +0800

    feat: dashboard init setup

--- lua/syfless/plugins/dashboard.lua
+++ lua/syfless/plugins/dashboard.lua
@@ -23,6 +23,8 @@ return {
       theme = "hyper",
       config = {
         header = vim.split(logo, "\n"),
+        project = { enable = false },
+        footer = {},
         shortcut = {
           { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
           {
@@ -32,18 +34,6 @@ return {
             group = "Label",
             action = "Telescope find_files",
             key = "f",
-          },
-          {
-            desc = " Apps",
-            group = "DiagnosticHint",
-            action = "Telescope app",
-            key = "a",
-          },
-          {
-            desc = " dotfiles",
-            group = "Number",
-            action = "Telescope dotfiles",
-            key = "d",
           },
         },
       },
