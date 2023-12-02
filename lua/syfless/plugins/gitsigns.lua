d4bc08aa1f37f1a86700796e2ef8430fd91775e3
syfless <syfless1999@gmail.com>
2023-12-02 16:10:07 +0800

    fix: <leader>gb => :Git blame

--- lua/syfless/plugins/gitsigns.lua
+++ lua/syfless/plugins/gitsigns.lua
@@ -2,18 +2,6 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("gitsigns").setup({
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        map("n", "<leader>bl", gs.blame_line)
-      end,
-    })
+    require("gitsigns").setup({})
   end,
 }
