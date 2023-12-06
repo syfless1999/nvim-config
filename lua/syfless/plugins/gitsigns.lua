85334a3f9e52f59ed505ce66cb6c966842caea17
syfless1999 <syfless1999@gmail.com>
2023-12-06 15:38:58 +0800

    feat: add <leader>bl

--- lua/syfless/plugins/gitsigns.lua
+++ lua/syfless/plugins/gitsigns.lua
@@ -2,6 +2,18 @@ return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("gitsigns").setup({})
+    require("gitsigns").setup({
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map("n", "<leader>bl", gs.blame_line)
+      end,
+    })
   end,
 }
