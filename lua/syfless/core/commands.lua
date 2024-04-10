d6432c89067ad8ba9d8b384ffecce2c6959b9825
syfless1999 <syfless1999@gmail.com>
2024-04-10 16:55:36 +0800

    vertical split open help doc use

--- lua/syfless/core/commands.lua
+++ lua/syfless/core/commands.lua
@@ -8,3 +8,8 @@ vim.api.nvim_create_user_command("RelPath", function()
   local path = vim.fn.expand("%:~:.")
   vim.notify(path)
 end, {})
+
+-- open help doc in vertical split use `:h`
+-- open help doc in horizontal split use `:H`
+vim.cmd("cnoreabbrev H h")
+vim.cmd("cnoreabbrev h vert h")
