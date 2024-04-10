e7948cb9bda64b6498d19100407097ca61d48b80
syfless1999 <syfless1999@gmail.com>
2024-04-10 11:54:20 +0800

    bufferlint init

---
+++ lua/syfless/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  dependencies = { ""}
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "slant",
+    },
+  },
+}
