44db1ac16c740c1c362bc2e3e828fb7fc0d30e30
syfless <syfless1999@gmail.com>
2024-04-10 13:06:42 +0800

    bufferline setup

--- lua/syfless/plugins/bufferline.lua
+++ lua/syfless/plugins/bufferline.lua
@@ -1,12 +1,14 @@
 return {
-  dependencies = { ""}
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "tabs",
+        style_preset = bufferline.style_preset.no_italic,
+      },
+    })
+  end,
 }
