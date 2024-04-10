ec43ac6563acb0dd709c210e66f80961c8380fc1
syfless1999 <syfless1999@gmail.com>
2024-04-10 11:53:39 +0800

    update basic

--- lua/syfless/core/keymaps.lua
+++ lua/syfless/core/keymaps.lua
@@ -18,8 +18,8 @@ keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
 keymap.set("n", "<leader>to", ":tabnew %<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<C-[>", ":tabp<CR>") --  go to previous tab
-keymap.set("n", "<C-]>", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>nu", ":set relativenumber!<CR>") -- toggle relativenumber
 
 -- buffer wider
