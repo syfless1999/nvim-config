b7dc0a46c51432a4eb28b83b6336d29ef9ee586a
syfless <syfless1999@gmail.com>
2024-04-25 00:42:39 +0800

    fix

--- lua/syfless/plugins/lsp/lspconfig.lua
+++ lua/syfless/plugins/lsp/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
+  -- event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
