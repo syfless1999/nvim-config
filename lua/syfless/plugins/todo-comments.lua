3e9dccfcdd89fbf19c3f9c5d09a157d06367828b
syfless <syfless1999@gmail.com>
2024-05-02 00:11:42 +0800

    fix: todo-comment fix event

--- lua/syfless/plugins/todo-comments.lua
+++ lua/syfless/plugins/todo-comments.lua
@@ -1,7 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  event = { "BufReadPre", "BufNewFile" },
+  event = 'VeryLazy',
   config = function()
     local todo_comments = require("todo-comments")
     local keymap = vim.keymap
