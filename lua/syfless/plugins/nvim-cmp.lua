5b7b577f65dd0553e95c94f241053d8ad385505e
syfless <syfless1999@gmail.com>
2024-05-04 17:23:52 +0800

    snip: keymap to jump insert

--- lua/syfless/plugins/nvim-cmp.lua
+++ lua/syfless/plugins/nvim-cmp.lua
@@ -25,6 +25,13 @@ return {
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
+    local keymap = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+    keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+    keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+    keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+    keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
