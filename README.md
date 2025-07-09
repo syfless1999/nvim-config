6f20b7f35da1410c72b40525d98b19e9f2cb5de6
syfless <shangyifan.fengshun@bytedance.com>
2025-07-09 20:48:58 +0800

    feat: auto ime

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ---
 
 > hello neovim
+
+
+
+## something extra
+
+1. use **auto ime**, you should install [macism](https://github.com/laishulu/macism)
+or you can disable it in `lua/syfless/core/keymaps.lua`
--- lua/syfless/core/keymaps.lua
+++ lua/syfless/core/keymaps.lua
@@ -12,15 +12,15 @@ keymap.set("n", "H", "0")
 keymap.set("n", "L", "$")
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v")                   -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s")                   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=")                   -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>")               -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew %<CR>") -- open new tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>to", ":tabnew %<CR>")            -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>")            -- close current tab
+keymap.set("n", "<leader>tp", ":tabp<CR>")                --  go to previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>")                -- go to next tab
 keymap.set("n", "<leader>nu", ":set relativenumber!<CR>") -- toggle relativenumber
 
 -- buffer wider
@@ -31,3 +31,28 @@ keymap.set("n", "<C-w>k", "5<C-w>+")
 
 -- fold
 keymap.set("n", "z{", "zfa{")
+
+-- auto ime
+local ime_autogroup = vim.api.nvim_create_augroup("ImeAutoGroup", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = ime_autogroup,
+  callback = function()
+    vim.system({ "macism" }, { text = true }, function(out)
+      -- store previous im
+      previous_im_on_mac = string.gsub(out.stdout, "\n", "")
+    end)
+
+    vim.cmd ":silent :!macism com.apple.keylayout.ABC"
+  end
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = ime_autogroup,
+  callback = function()
+    if previous_im_on_mac then
+      vim.cmd(":silent :!macism " .. previous_im_on_mac)
+    end
+    previous_im_on_mac = nil
+  end
+})
